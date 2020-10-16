39e5294f9091cb1ed13c368f432ba490995ef3c0
vig3dev <vig3.dev@gmail.com>
2020-10-16 02:25:54 -0700

    homnepage: title, glow, js color switch, links

---
+++ themes/viggy/static/js/scripts.js
@@ -0,0 +1,10 @@
+function clickChange() {
+  let changer = document.getElementById("changer");
+  changer.style.color = "black";
+}
+
+function clickBack() {
+  let changer = document.getElementById("changer");
+  changer.style.color = "white"
+}
+console.log("hello")
