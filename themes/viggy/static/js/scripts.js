856a28482cc74629e85c8b6b49f684bc04b3f8a1
vig3dev <vig3.dev@gmail.com>
2020-10-22 01:26:27 -0700

    homepage looking good, scroll working, page fits

--- themes/viggy/static/js/scripts.js
+++ themes/viggy/static/js/scripts.js
@@ -1,20 +1,5 @@
-function clickChange(name) {
-  ScrollToResolver(document.getElementById(name));
+function clickChange() {
+  let changer = document.getElementById("stagePage");
+  changer.style.color = "black";
 }
-
-function ScrollToResolver(elem) {
-  var jump = parseInt(elem.getBoundingClientRect().top * .2);
-  document.body.scrollTop += jump;
-  document.documentElement.scrollTop += jump;
-  //lastjump detects anchor unreachable, also manual scrolling to cancel animation if scroll > jump
-  if (!elem.lastjump || elem.lastjump > Math.abs(jump)) {
-    elem.lastjump = Math.abs(jump);
-    setTimeout(function() {
-      ScrollToResolver(elem);
-    }, "100");
-  } else {
-    elem.lastjump = null;
-  }
-}
-
 console.log("hello")
