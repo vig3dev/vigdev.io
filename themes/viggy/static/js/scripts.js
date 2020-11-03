97d020be1273c71c51de53a4b80582473d56144f
vig3dev <vig3.dev@gmail.com>
2020-11-02 23:22:32 -0600

    retry js

--- themes/viggy/static/js/scripts.js
+++ themes/viggy/static/js/scripts.js
@@ -1,10 +1,1 @@
-function clickChange() {
-  let changer = document.getElementById("stagePage");
-  changer.style.color = "black";
-}
-
-function aboutJs() {
-  let changer = document.getElementById()
-}
-
 console.log("hello")
