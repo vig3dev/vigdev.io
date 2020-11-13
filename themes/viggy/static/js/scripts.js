3d8c4e29212cdc444085af14a92d467630813ff3
vig3dev <vig3.dev@gmail.com>
2020-11-12 21:41:38 -0600

    js tabs work now

--- themes/viggy/static/js/scripts.js
+++ themes/viggy/static/js/scripts.js
@@ -1,9 +1,16 @@
-console.log("hbldfs")
+console.log("hello")
 
-function changer() {
-  const mainPage = document.getElementById("mainPage");
-  const about = document.getElementById("about").innerHTML;
-  const contact = document.getElementById("contact");
-
-  document.getElementById("changer").innerHTML = about;
+function openContent(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
+
